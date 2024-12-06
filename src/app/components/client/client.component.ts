085b92b2bafaf7064870c96b229c3ed834152d50
aayushchhetri2 <aayush.b.lamichhane@gmail.com>
2024-12-06 20:13:56 +0545

    Resuable Components

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -6,10 +6,12 @@ import { APIResponseModel } from '../../model/interface/role';
 import { AsyncPipe, DatePipe, JsonPipe, UpperCasePipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Constant } from '../../constant/Constant';
+import { AlertComponent } from "../reusableComponents/alert/alert.component";
+import { MyButtonComponent } from "../reusableComponents/my-button/my-button.component";
 
 @Component({
   selector: 'app-client',
-  imports: [FormsModule, UpperCasePipe, DatePipe, JsonPipe, AsyncPipe],
+  imports: [FormsModule, UpperCasePipe, DatePipe, JsonPipe, AsyncPipe, AlertComponent, MyButtonComponent],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
@@ -29,7 +31,7 @@ export class ClientComponent implements OnInit{
     })
   }
 
-  onSave(){
+  onSave(data:string){
     this.clientService.addUpdate(this.clientObj).subscribe((res: APIResponseModel) =>{
       if(res.result){
         alert("Client created successfully")
