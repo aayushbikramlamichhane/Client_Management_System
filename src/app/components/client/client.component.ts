be021893431ff1dcfd60ee0d3168eba4949c1bcf
aayushchhetri2 <aayush.b.lamichhane@gmail.com>
2024-12-06 16:37:17 +0545

    Pipe

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -3,20 +3,25 @@ import { Client } from '../../model/class/Client';
 import { FormsModule } from '@angular/forms';
 import { ClientService } from '../../services/client.service';
 import { APIResponseModel } from '../../model/interface/role';
+import { AsyncPipe, DatePipe, JsonPipe, UpperCasePipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-client',
-  imports: [FormsModule],
+  imports: [FormsModule, UpperCasePipe, DatePipe, JsonPipe, AsyncPipe],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
 export class ClientComponent implements OnInit{
+  currentDate: Date = new Date();
   clientObj: Client = new Client();
   clientList : Client[] = [];
   clientService = inject(ClientService);
 
+  userList$: Observable<any> = new Observable<any>;
   ngOnInit(): void {
     this.loadClient();
+    this.userList$ = this.clientService.getAllUser();
   }
   loadClient(){
     this.clientService.getAllClient().subscribe((res:APIResponseModel) => {
